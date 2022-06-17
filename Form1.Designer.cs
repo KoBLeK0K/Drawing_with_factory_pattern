9d69592015b9f5c2d2760765697386545c3f2360
KoBLeK0K <75327213+KoBLeK0K@users.noreply.github.com>
2022-06-17 21:00:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace _6Drawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SizeOfFigure = new System.Windows.Forms.TextBox();
+            this.xAxis = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.yAxis = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.anglesOfMult = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Construct = new System.Windows.Forms.Button();
+            this.drawingSpace = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingSpace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Треугольник",
+            "Квадрат",
+            "Круг",
+            "Многоугольник"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 352);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Тип фигуры";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 334);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Размер";
+            // 
+            // SizeOfFigure
+            // 
+            this.SizeOfFigure.Location = new System.Drawing.Point(150, 352);
+            this.SizeOfFigure.Name = "SizeOfFigure";
+            this.SizeOfFigure.Size = new System.Drawing.Size(100, 23);
+            this.SizeOfFigure.TabIndex = 5;
+            this.SizeOfFigure.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SizeOfFigure_KeyPress);
+            // 
+            // xAxis
+            // 
+            this.xAxis.Location = new System.Drawing.Point(271, 352);
+            this.xAxis.Name = "xAxis";
+            this.xAxis.Size = new System.Drawing.Size(100, 23);
+            this.xAxis.TabIndex = 7;
+            this.xAxis.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.xAxis_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Координаты середины";
+            // 
+            // yAxis
+            // 
+            this.yAxis.Location = new System.Drawing.Point(388, 352);
+            this.yAxis.Name = "yAxis";
+            this.yAxis.Size = new System.Drawing.Size(100, 23);
+            this.yAxis.TabIndex = 8;
+            this.yAxis.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.yAxis_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(271, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Х";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(388, 334);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Y";
+            // 
+            // anglesOfMult
+            // 
+            this.anglesOfMult.Enabled = false;
+            this.anglesOfMult.Location = new System.Drawing.Point(12, 406);
+            this.anglesOfMult.Name = "anglesOfMult";
+            this.anglesOfMult.Size = new System.Drawing.Size(121, 23);
+            this.anglesOfMult.TabIndex = 11;
+            this.anglesOfMult.TextChanged += new System.EventHandler(this.anglesOfMult_TextChanged);
+            this.anglesOfMult.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.anglesOfMult_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 388);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(201, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Количество углов многоугольника";
+            // 
+            // Construct
+            // 
+            this.Construct.Location = new System.Drawing.Point(548, 351);
+            this.Construct.Name = "Construct";
+            this.Construct.Size = new System.Drawing.Size(223, 87);
+            this.Construct.TabIndex = 13;
+            this.Construct.Text = "Построить фигуру";
+            this.Construct.UseVisualStyleBackColor = true;
+            this.Construct.Click += new System.EventHandler(this.Construct_Click);
+            // 
+            // drawingSpace
+            // 
+            this.drawingSpace.Location = new System.Drawing.Point(12, 12);
+            this.drawingSpace.Name = "drawingSpace";
+            this.drawingSpace.Size = new System.Drawing.Size(776, 304);
+            this.drawingSpace.TabIndex = 14;
+            this.drawingSpace.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(652, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(136, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Наименование фигуры";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.drawingSpace);
+            this.Controls.Add(this.Construct);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.anglesOfMult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.yAxis);
+            this.Controls.Add(this.xAxis);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SizeOfFigure);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.drawingSpace)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Construct;
+        private System.Windows.Forms.PictureBox drawingSpace;
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.TextBox SizeOfFigure;
+        public System.Windows.Forms.TextBox xAxis;
+        public System.Windows.Forms.TextBox yAxis;
+        public System.Windows.Forms.TextBox anglesOfMult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6Drawing
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+        public int getHeight()
+        {
+            return drawingSpace.Height;
+        }
+
+        public int getWidth()
+        {
+            return drawingSpace.Width;
+        }
+        public void anglesOfMult_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+            
+        }
+        public void xAxis_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+
+            
+    }
+        public void yAxis_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+        public void SizeOfFigure_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Construct_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.Text != "" && xAxis.Text != "" && yAxis.Text != "" && SizeOfFigure.Text != "")
+            {
+                
+                ShapeFactory factory = new ShapeFactory();
+                Shape shapePolygon = ShapeFactory.GetShape(comboBox1.Text);
+
+                drawingSpace.Image = shapePolygon.Draw(SizeOfFigure.Text, xAxis.Text, yAxis.Text, anglesOfMult.Text);
+                label7.Text = shapePolygon.Descriptor();
+                
+            }
+            
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Многоугольник")
+            {
+                anglesOfMult.Enabled = true;
+                
+            }
+            else
+            {
+                anglesOfMult.Clear();
+                anglesOfMult.Enabled = false;
+            }
+        }
+
+        private void anglesOfMult_TextChanged(object sender, EventArgs e)
+        {
+            if(Convert.ToInt32(anglesOfMult.Text) <= 4 )
+            {
+                Construct.Enabled = false;
+                MessageBox.Show("Количество углов многоугольника не может быть меньше 5", "НЕВОЗМОЖНО ПОСТРОИТЬ");
+                
+            }
+            else 
+            {
+                Construct.Enabled = true;
+            }
+
+        }
+    }
+
+    public class ShapeFactory
+    {
+        public static Shape GetShape(string figure)
+        {
+            switch (figure)
+            {
+                case "Многоугольник":
+                    return new Pentagon();
+                case "Квадрат":
+                    return new Rectangle();
+                case "Треугольник":
+                    return new Triangle();
+                default: return new Ellipse();
+            }
+
+        }
+    }
+    public abstract class Shape
+    {
+        public int x1;
+        public int y1;
+
+        public SolidBrush pen;
+        public Pen myPen;
+
+        public Shape()
+        {
+            x1 = 0;
+            y1 = 0;
+            pen = new SolidBrush(Color.White);
+            myPen = new Pen(Color.Blue);
+        }
+        abstract public Image Draw(string size, string xDot, string yDot, string MultiAngles);
+        abstract public string Descriptor();
+    }
+
+    class Ellipse : Shape
+    {
+        
+        public override Image Draw(string size, string xDot, string yDot, string MultiAngles)
+        {
+            Form1 form1 = new Form1();
+
+            int shapeSize = Convert.ToInt32(size);
+            int x2 = Convert.ToInt32(xDot);
+            int y2 = Convert.ToInt32(yDot);
+
+            Bitmap pictureBox = new (form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawEllipse(myPen, x1+x2 - shapeSize/2, y1+y2 - shapeSize/2, shapeSize, shapeSize);
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Circle";
+        }
+    }
+
+
+    class Triangle : Shape
+    {
+
+        public override Image Draw(string size, string xDot, string yDot, string MultiAngles)
+        {
+            Form1 form1 = new Form1();
+
+            int shapeSize = Convert.ToInt32(size);
+            int x2 = Convert.ToInt32(xDot);
+            int y2 = Convert.ToInt32(yDot);
+
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+
+            PointF p1 = new PointF(1*shapeSize + x2, 1+ y2 - shapeSize/2);
+            PointF p2 = new PointF(2+ x2 - shapeSize/2, 2*shapeSize+ y2);
+            PointF p3 = new PointF(2+ x2 - shapeSize/2, 1+ y2-shapeSize/2);
+            PointF[] curvePoints =
+            {
+                p1,p2,p3
+            };
+            graph.DrawPolygon(myPen, curvePoints);
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Triangle";
+        }
+    }
+
+    class Rectangle : Shape
+    {
+
+        public override Image Draw(string size, string xDot, string yDot, string MultiAngles)
+        {
+            Form1 form1 = new Form1();
+            int shapeSize = Convert.ToInt32(size);
+            int x2 = Convert.ToInt32(xDot);
+            int y2 = Convert.ToInt32(yDot);
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawRectangle(myPen, x1+x2 - shapeSize/2, y1+y2 - shapeSize/2, shapeSize, shapeSize);
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Rectangle";
+        }
+    }
+
+    class Pentagon : Shape
+    {
+        public override Image Draw(string size, string xDot, string yDot, string MultiAngles)
+        {
+
+            Form1 form1 = new Form1();
+
+            int shapeSize = Convert.ToInt32(size);
+            int x2 = Convert.ToInt32(xDot);
+            int y2 = Convert.ToInt32(yDot);
+            int sumDots = Convert.ToInt32(MultiAngles);
+
+            List<Point> points = new List<Point>();
+            points.Clear();
+
+            double R = shapeSize / (2 * Math.Sin(Math.PI / sumDots));
+
+            for(double angle = 0f; angle <= 2*Math.PI; angle+=Math.PI*2/sumDots)
+            {
+                int x = (int)(R * Math.Cos(angle));
+                int y = (int)(R * Math.Sin(angle));
+                points.Add(new Point((int)R + x + x2 - shapeSize, (int)R + y + y2-shapeSize));
+            }
+
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawPolygon(myPen, points.ToArray());
+            return pictureBox;
+        }
+        public override string Descriptor()
+        {
+            return "Graph";
+        }
+    }
+}
